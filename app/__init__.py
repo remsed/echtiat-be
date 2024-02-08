7c96f711949af15bf57c13de247df184a87c0104
Ruslan M <19166472+remsed@users.noreply.github.com>
2024-02-08 23:47:51 +0100

    Install SQLAlchemy. Add initial user model.

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
+    db = SQLAlchemy(app)
+    db_migrate = Migrate(app, db)
+
     from app.cli import cli_bp
     app.register_blueprint(cli_bp)
+
+    from app.models import user
 
     @app.route('/')
     def main_page():
---
+++ app/models/user.py
@@ -0,0 +1,10 @@
+from app import db
+from sqlalchemy import Column, Integer, String, Float
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String, unique=True)
+    password = Column(String)
