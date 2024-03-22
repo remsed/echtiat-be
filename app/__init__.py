307c44769ffebfa3bfb3dbb9e679990061c13fda
Ruslan M <19166472+remsed@users.noreply.github.com>
2024-03-22 19:03:26 +0100

    Add user registration and login functionality

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,17 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+from os import environ
 #from app.models import user
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.config['JWT_SECRET_KEY'] = environ.get('JWT_SECRET_KEY')
 
 db = SQLAlchemy(app)
 db_migrate = Migrate(app, db)
+jwt = JWTManager(app)
 
 from app.cli import cli_bp
 app.register_blueprint(cli_bp)
--- app/core/main.py
+++ app/core/main.py
@@ -1,5 +1,57 @@
-from app import app
+from app import app, db
+from flask import request, jsonify
+from app.models.user import User
+from flask_jwt_extended import create_access_token, jwt_required
 
 @app.route('/')
+@jwt_required()
 def main_page():
     return '<h1>Main page</h1>'
+
+@app.route('/register', methods=['POST'])
+def register():
+    if request.is_json:
+        email = request.json['email']
+        user_account = User.query.filter_by(email=email).first()
+        if user_account:
+            return jsonify(message='User account already exists!'), 409
+        else:
+            first_name = request.json['first_name']
+            last_name = request.json['last_name']
+            email = request.json['email']
+            password = request.json['password']
+            user = User(fname=first_name, lname=last_name, email=email, password=password)
+            db.session.add(user)
+            db.session.commit()
+            return jsonify(message='User account has been successfully created!'), 201
+    else:
+        email = request.form['email']
+        user_account = User.query.filter_by(email=email).first()
+        if user_account:
+            return jsonify(message='User account already exists!'), 409
+        else:
+            first_name = request.form['first_name']
+            last_name = request.form['last_name']
+            email = request.form['email']
+            password = request.form['password']
+            user = User(fname=first_name, lname=last_name, email=email, password=password)
+            db.session.add(user)
+            db.session.commit()
+            return jsonify(message='User account has been successfully created!'), 201
+    
+@app.route('/login', methods=['POST'])
+def login():
+    if request.is_json:
+        email = request.json['email']
+        password = request.json['password']
+    else:
+        email = request.form['email']
+        password = request.form['password']
+    
+    user_account = User.query.filter_by(email=email, password=password).first()
+
+    if user_account:
+        user_jwt_token = create_access_token(identity=email)
+        return jsonify(message='Login is successful!', access_token=user_jwt_token)
+    else:
+        return jsonify(message='Email or password is incorrect!'), 401
